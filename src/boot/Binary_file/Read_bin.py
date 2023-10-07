2ac88e703f3f5e4808c0a4546ca65c02adf68989
RogueTMs <roguecyb@gmail.com>
2023-10-07 16:44:12 +0700

    Update Read_bin.py
    Still not work...

--- src/boot/Binary_file/Read_bin.py
+++ src/boot/Binary_file/Read_bin.py
@@ -1,17 +1,10 @@
-#check_sum = 0
-#
-#with open('src/boot/Binary_file/binary.bin', "rb") as f:
-#    bin_data = f.read()
-#    for i in range(0, len(bin_data)):
-#        byte = bin_data[i]
-#        check_sum += byte
-#
-#
-#print(hex(check_sum%2**8))
+check_sum = 0
 
-
-add = 0
-with open('src/boot/Binary_file/binary.bin', 'rb') as f:
+with open('src/boot/boot.bin', "rb") as f:
     bin_data = f.read()
-    print(sum(bin_data)%2**8)
-    
+    for i in range(0, len(bin_data)):
+        byte = bin_data[i]
+        check_sum = (check_sum + byte)
+
+
+print(hex(check_sum%2**8))
