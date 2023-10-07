de366adec2694a3f537c26c93964c178030eda60
RogueTMs <roguecyb@gmail.com>
2023-10-07 16:45:37 +0700

    Update Read_bin.py
    Finally

--- src/boot/Binary_file/Read_bin.py
+++ src/boot/Binary_file/Read_bin.py
@@ -1,6 +1,6 @@
 check_sum = 0
 
-with open('src/boot/boot.bin', "rb") as f:
+with open('src/boot/boot.img', "rb") as f:
     bin_data = f.read()
     for i in range(0, len(bin_data)):
         byte = bin_data[i]
